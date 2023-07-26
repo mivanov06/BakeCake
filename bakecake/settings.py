719ee7e080add7b9fbc64b84986a059e3d2d18aa
mivanov06 <mivanov06@yandex.ru>
2023-07-26 21:35:35 +1000

    remove images to media

--- bakecake/settings.py
+++ bakecake/settings.py
@@ -117,6 +117,9 @@ STATICFILES_DIRS = [
     BASE_DIR.joinpath('static'),
 ]
 
+MEDIA_ROOT = BASE_DIR.joinpath('media')
+MEDIA_URL = 'media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
